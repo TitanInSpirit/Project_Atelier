89bd6e6570e39d098031f6532c212dce992b6625
Feifei Liang <mayliang021@gmail.com>
2022-07-07 11:09:11 -0500

    initial setup

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+// Huzzah for jsx!
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+root.render(<App />);
