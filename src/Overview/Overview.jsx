5af3b10671fe912ae4fe4612491d1afea359cd29
Symphon-y <travisredden@outlook.com>
2022-07-07 17:19:23 -0500

    added Overview component folder, file, and diagram

---
+++ src/Overview/Overview.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props), (this.state = {});
+  }
+
+  render() {
+    return (
+      <div>
+        <p> This is where the overview goes</p>
+      </div>
+    );
+  }
+}
+
+export default Overview;
