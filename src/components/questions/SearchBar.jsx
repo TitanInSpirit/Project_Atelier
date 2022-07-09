122d84524b14c5ac65d43211c2e139562b15fead
sbirvin1s <sbirvin1s@gmail.com>
2022-07-08 21:06:37 -0500

    adds skeleton and diagram for Questions and Answers component

---
+++ src/components/questions/SearchBar.jsx
@@ -0,0 +1,14 @@
+/*==================== EXTERNAL MODULES ====================*/
+import React from 'react';
+
+/*==================== INTERNAL MODULES ====================*/
+
+function SearchBar = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+/*==================== EXPORTS ====================*/
+export default SearchBar;
