9de4e6dab3509c1b714506fba13b47eef8f65514
sbirvin1s <sbirvin1s@gmail.com>
2022-07-09 14:16:15 -0500

    changes double quotes on strings to single quotes

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Overview from "./components/overview/Overview.jsx";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Overview from './components/overview/Overview.jsx';
 import RatingsAndReviews from './components/ratingsAndReviews/RatingsAndReviews.jsx';
 import Questions from './components/questions/Questions.jsx';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 
 const App = () => {
   return (
