99c323a602f7d4178f06463520986010581ae6b3
Feifei Liang <mayliang021@gmail.com>
2022-07-09 13:46:29 -0500

    update index file into single quote

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Overview from "./components/overview/Overview.jsx";
-<<<<<<< HEAD
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Overview from './components/overview/Overview.jsx';
 import RatingsAndReviews from './components/ratingsAndReviews/RatingsAndReviews.jsx';
 import '../public/stylesheets/style.css'
-=======
-import RatingsAndReviews from "./components/ratingsAndReviews/RatingsAndReviews.jsx";
->>>>>>> ac6d72d01e1affa63d7e652b26fe4d3f88f08c55
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 
 const App = () => {
   return (
