2e68eda86f4e0fc39204e1f5a90fd8ec2b0c0e1c
Symphon-y <travisredden@outlook.com>
2022-07-12 18:23:50 -0500

    updated overview component, added icon library, refactored state to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,12 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      products: []
+      products: [],
+      all_styles:  [],
+      current_product: {},
+      current_style: {},
+      total_reviews: 0,
+      average_reviews: 0,
     }
 
   }
@@ -22,13 +27,55 @@ class App extends React.Component {
   }
 
   getAllProducts = () => {
-    axios.get('http://localhost:3002/products')
+    axios.get('http://localhost:3001/products')
     .then((response) => {
-      this.setState({products: response.data})
-      console.log(JSON.stringify(this.state.products[0]))
+      this.setState({products: response.data, current_product: response.data[0]})
+      this.getInfo(response.data[0]['id'])
+      this.getStyles(response.data[0]['id'])
     })
     .catch((err) => {
       console.log(err)
+    })
+  }
+
+  setCurrentStyle = (styleId, styles) => {
+    styleId = styleId || null;
+    if (!styleId) {
+      styles.map((style) => {
+        style['default?'] ? this.setState({current_style: style}) : null
+      })
+    } else if (styleId) {
+      this.state.all_styles.map((style) => {
+        style.style_id === styleId ? this.setState({current_style: style}) : null
+      })
+    }
+  }
+
+  getStyles = (productId) => {
+    return axios.post('http://localhost:3001/styles', {productId})
+    .then((results) => {
+      this.setCurrentStyle(null, results.data.results)
+      this.setState({all_styles: results.data.results})
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  getInfo = (productId) => {
+    let payload = productId
+    axios.post('http://localhost:3001/reviews', {productId})
+    .then((res) => {
+      let response = res.data
+      let reviewTotal = 0;
+      let increment = response.results.map((review) => {
+        reviewTotal+= review.rating
+      })
+      let average = reviewTotal / response.count
+      this.setState({total_reviews: response.count, average_reviews: average})
+    })
+    .catch((err) => {
+      console.log('Axios Post Error, ', err)
     })
   }
 
@@ -36,7 +83,15 @@ class App extends React.Component {
     return (
       <div>
         <h1> Header: Hello World!!!!!!</h1>
-        <Overview products={this.state.products} getAllProducts={this.getAllProducts}/>
+        <Overview
+        products={this.state.products}
+        getAllProducts={this.getAllProducts}
+        all_styles={this.state.all_styles}
+        current_product={this.state.current_product}
+        current_style={this.state.current_style}
+        total_reviews={this.state.total_reviews}
+        average_reviews={this.state.average_reviews}
+        />
         <Questions products={this.state.products} getAllProducts={this.getAllProducts}/>
         <RatingsAndReviews products={this.state.products} getAllProducts={this.getAllProducts}/>
       </div>
