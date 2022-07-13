865b7b590ec8212e29b4c4d6ad808e366f729ab8
sbirvin1s <sbirvin1s@gmail.com>
2022-07-12 22:24:47 -0500

    adds simple jest test to check if component was rendered

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -5,68 +5,6 @@ import axios from 'axios';
 
 const Overview = (props) => {
 
-<<<<<<< questions
-    });
-  }
-
-  componentDidMount = () => {
-    this.getAllProducts()
-  }
-
-  getAllProducts = () => {
-    axios.get('http://localhost:3001/products')
-    .then((response) => {
-      this.setState({products: response.data, current_product: response.data[0]})
-      this.getInfo(response.data[0]['id'])
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
-
-  setCurrentStyle = (styleId, productId) => {
-    styleId = styleId || null;
-    productId = productId || null;
-    // get current style id
-    // get original price
-    // get all skus (array of objects)
-    // get sale price
-    // get all photos
-  }
-
-  getInfo = (productId) => {
-    let payload = productId
-    axios.post('http://localhost:3001/reviews', {productId}).then((res) => {
-      let response = res.data
-      console.log(`Reviews for Product: ${productId}`)
-      console.log(res.data)
-      // get total number of reviews
-      // get individual ratings
-        // do math to find average
-      // put all of these in state
-    }).catch((err) => {
-      console.log('Axios Post Error, ', err)
-    })
-
-    axios.post('http://localhost:3001/styles', {productId}).then((res) => {
-      let response = res.data
-      this.setState({all_styles: res.data.results})
-      this.setCurrentStyle(productId)
-    }).catch((err) => {
-      console.log('Axios Post Error, ', err)
-    })
-  }
-
-  render() {
-    if (this.state.products.length === 0) {
-      return (
-      <div>
-        <div className="loading">
-            <div>
-            </div>
-            <div>
-            </div>
-=======
   if (props.products.length === 0) {
     return (
     <div>
@@ -74,7 +12,6 @@ const Overview = (props) => {
           <div>
           </div>
           <div>
->>>>>>> main
           </div>
         </div>
       </div>
