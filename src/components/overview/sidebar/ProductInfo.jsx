bd9a4217c5a03237556d344c0ae73f4210fac6c3
Symphon-y <travisredden@outlook.com>
2022-07-14 17:38:48 -0500

    added prices to ProductInfo.jsx

--- src/components/overview/sidebar/ProductInfo.jsx
+++ src/components/overview/sidebar/ProductInfo.jsx
@@ -6,6 +6,7 @@ function ProductInfo(props) {
     <div className="product_category_title product_info">
       <span className="product_category product_info"><h2>Category: {props.current_product.category}</h2></span>
       <span className="product_title product_info"><h1>{props.current_product.name}</h1></span>
+      <div className="price">{props.current_style.sale_price ? <span><h3 style={{color: 'red'}}>{props.current_style.sale_price }</h3> <s>{props.current_style.original_price}</s></span>: <span>{props.current_style.original_price}</span>}</div>
     </div>
   </div>;
 }
