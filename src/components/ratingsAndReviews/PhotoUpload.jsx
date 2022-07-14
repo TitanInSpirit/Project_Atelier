0b1dc852020e584c329489bc5ae1a5e144690912
Feifei Liang <mayliang021@gmail.com>
2022-07-14 17:06:18 -0500

    fix photo upload with choosing multiple ones

--- src/components/ratingsAndReviews/PhotoUpload.jsx
+++ src/components/ratingsAndReviews/PhotoUpload.jsx
@@ -7,49 +7,38 @@ const PhotoUpload = ({setPhotos, photos}) => {
   let returnUrl = []
 
   const uploadImage = e => {
-    // const {files} = e.target;
-    // console.log('files',files)
-    // return arr.map(file => {
-    //   const formData = new FormData();
-    //   formData.append('file', file);
-    //   formData.append('upload_preset', 'FEC-NewReviewPhotosUploads')
-    //   return axios.post('https://api.cloudinary.com/v1_1/may6688/image/upload', formData)
-    //   .then(res => returnUrl.push(res.data.url))
-    // })
+    const allUrls = arr.map(file => {
       const formData = new FormData();
-      formData.append('file', imageSelected);
+      formData.append('file', file);
       formData.append('upload_preset', 'FEC-NewReviewPhotosUploads')
       return axios.post('https://api.cloudinary.com/v1_1/may6688/image/upload', formData)
-      .then(res => setPhotos([...photos, res.data.url]))
+      .then(res => returnUrl.push(res.data.url))
+      .then(() => {return returnUrl})
+      .then((returnUrl) => setPhotos([...photos, ...returnUrl]))
+    })
   };
 
-  // useEffect(() => {
-  //   setPhotos([...photos, ...returnUrl])
-  // }, [returnUrl])
-
-
-  // const handleChange = e => {
-  //   console.log(e.target.files)
-  //   console.log('file 1', e.target.files[0])
-  //   console.log('file 2', e.target.files[1])
-  //   const {length} = e.target.files;
-  //   for (let i = 0; i < length; i++) {
-  //     // setImageSelected([...imageSelected, e.target.files[i]])
-  //     arr.push(e.target.files[i])
-  //   }
-  // }
+  const handleChange = e => {
+    console.log(e.target.files)
+    console.log('file 1', e.target.files[0])
+    console.log('file 2', e.target.files[1])
+    const {length} = e.target.files;
+    for (let i = 0; i < length; i++) {
+      arr.push(e.target.files[i])
+    }
+  }
 
 
   return (
     <div>
-      {console.log('arrrrr is', arr)}
+      {/* {arr.length > 0 && console.log('arrrrr is', arr)}
+      {console.log('returnUrl', {returnUrl})} */}
       {/* {console.log('imageSelected', imageSelected)} */}
-      {console.log('photos', photos)}
+      {/* {console.log('photos', photos)} */}
       {photos.length < 5 && (
         <input
         type='file'
-        onChange={e => setImageSelected(e.target.files[0])}
-        // onChange={handleChange}
+        onChange={handleChange}
         multiple
       />
       )}
