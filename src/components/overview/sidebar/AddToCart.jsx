8a0703cf9591fec61c0c4ba39f4a71fe500c65d6
Symphon-y <travisredden@outlook.com>
2022-07-14 17:10:20 -0500

    added add to cart, and favorite buttons

--- src/components/overview/sidebar/AddToCart.jsx
+++ src/components/overview/sidebar/AddToCart.jsx
@@ -13,24 +13,32 @@ function AddToCart(props) {
           let size = skus[item].size
           sizes.push({size:size, sku: item})
         }
-        return (<div className="add_to_cart_container">
-          <select name="select_size" id="sizes" className="overview_button select_size" onChange={props.setCurrentSku}>
-          <option value="default" disabled selected >SELECT SIZE</option>
-          {sizes.map((size) => {
-          return(
-            <option value={size.sku} id={size.sku} key={size.sku}>{size.size}</option>
-            )
-         })}
-        </select>
-        <select name="select_size" id="sizes" className="overview_button select_size">
-          <option value="default" disabled selected >SELECT QUANTITY</option>
-          {availabilityArray.map((quantity) => {
-          return(
-            <option value={quantity} id={quantity} key={quantity}>{quantity}</option>
-            )
-         })}
-        </select>
+        return (
+        <form className="add_to_cart_container">
+          <div className="size_and_quantity_container">
+            <select name="select_size" id="sizes" className="overview_button select_size" onChange={props.setCurrentSku}>
+              <option value="default" disabled selected >SELECT SIZE</option>
+              {sizes.map((size) => {
+              return(
+                <option value={size.sku} id={size.sku} key={size.sku}>{size.size}</option>
+                )
+               })}
+          </select>
+          <select name="select_quantity" id="quantity" className="overview_button select_quantity">
+            <option value="default" disabled selected >-</option>
+            {availabilityArray.map((quantity) => {
+            return(
+              <option value={quantity} id={quantity} key={quantity}>{quantity}</option>
+              )
+          })}
+          </select>
+          <div className="add_to_cart_favorite_container">
+          <button type="submit" className="add_to_cart_btn overview_button">ADD TO BAG</button>
+          <button className="overview_favorite_btn overview_button">⭐</button>
+          </div>
         </div>
+
+        </form>
         )
     }
 }
