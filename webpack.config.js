44e46e70572c086f665e364cb1a6f65a44c2abae
Symphon-y <travisredden@outlook.com>
2022-07-20 22:38:36 -0500

    added smooth growth animation for big picture

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,34 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.jsx",
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader','css-loader']
-      },
-    ]
-  },
-  // [devtool] this is an additional source map that will let the browser know what files are running our code.
-  // Helps with error tracing. Without it we will not know where our errors are coming from because it will state that everything inside the bundle file.
-  devtool: "eval-cheap-module-source-map",
-  // [devServer] configuration for the live server including port
-  devServer: {
-    // [static] config for how what to serve
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports={
+    mode: "development",
+    entry: "./src/index",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
     },
-    compress: true,
-    // [port] what port on our local machine to run the dev server
-    port: 3001,
-  }
+    target: "web",
+    devServer: {
+        port: "9500",
+        static: ["./public"],
+        open: true,
+        hot: true ,
+        liveReload: true
+    },
+    resolve: {
+        extensions: ['.js','.jsx','.json']
+    },
+    module:{
+        rules: [
+            {
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              use:  'babel-loader'
+            },
+            {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
